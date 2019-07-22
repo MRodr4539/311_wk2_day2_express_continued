fe0b15b3d108f79e0b9b16b56a686707eda829af
MRodr4539 <miranda.rodriguez4539@yahoo.com>
2019-07-21 21:45:31 -0500

    comments and contacts

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,25 @@
 const products = require('../data/products')
 
 const list = (req, res) => {
-    res.json(products)
+  res.json(products)
+}
+
+const show = (req, res) => {
+  const product = products.find(function(item){
+    return item._id == req.params.id
+    })
+    res.json(product)
+}
+
+const create = (req, res) => {
+  const newProduct = {
+    _id: products.length +1,
+    name: req.body.name,
+    description: req.body.description
+  }
+    products.push(newProduct)
 }
 
 module.exports = { 
-    list
+  list, show, create
 }
---
+++ data/comments.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+      "_id": 1,
+      "body": "Stoked about becoming a Dev women!!",
+      "postId": 1
+    },
+    {
+      "_id": 2,
+      "body": "I love slice, bubble sort, !",
+      "postId": 1
+    },
+    {
+      "_id": 3,
+      "body": "Best coding school is Austin Coding Academy in ATX!",
+      "postId": 1
+    },
+    {
+      "_id": 4,
+      "body": "I enjoy learning new coding news everyday!",
+      "postId": 1
+    }
+  ];
